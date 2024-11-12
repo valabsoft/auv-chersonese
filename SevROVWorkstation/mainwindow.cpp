ab83b6cbf9b748281c3d3f39b5530097fa990756
Kirill Paraev <kir23rus2013@yandex.ru>
2024-11-12 15:12:00 +0300

    fixed video files deletion on lowering limit of saved files

--- SevROVWorkstation/mainwindow.cpp
+++ SevROVWorkstation/mainwindow.cpp
@@ -831,24 +831,25 @@ void MainWindow::recordVideo(std::vector<cv::Mat> frames, int recordInterval, cv
     else
     {
         int videoFileCount = 0;
-        filesystem::directory_entry oldestVideoFile;
         for (const auto & entry : std::filesystem::directory_iterator(pathToVideoDirectory)) {
             //std::cout << entry.path() << std::endl; // с каким файлом/папкой имеем дело
             if (!entry.is_directory()){
                 videoFileCount++;
-                if (videoFileCount == 1)
-                {
-                    // в интернете видел инфо, что std::filesystem::directory_iterator
-                    // не по порядку итерирует файлы. во время тестов у меня это не проявилось -
-                    // всегда проходит по возрастанию в алфавитном порядке, если сойдет с ума -
-                    // копать здесь
-                    oldestVideoFile = entry;
-                }
             }
         }
-        if (videoFileCount >= _appSet.STORED_VIDEO_FILES_LIMIT)
+        while (videoFileCount >= _appSet.STORED_VIDEO_FILES_LIMIT)
         {
-            std::filesystem::remove(oldestVideoFile);
+            for (const auto & entry : std::filesystem::directory_iterator(pathToVideoDirectory)) {
+                if (!entry.is_directory()){
+                    std::filesystem::remove(entry);
+                    videoFileCount--;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
     }
 
